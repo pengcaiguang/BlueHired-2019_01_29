d01f880a0b2566806aa74af71c5070f6e54be39d
xingxiaoliang <1018328249@qq.com>
2018-08-27 17:07:44 +0800

    基础界面

---
+++ BlueHired/Class/Main/Controller/LPMainVC.h
@@ -0,0 +1,13 @@
+//
+//  LPMainVC.h
+//  BlueHired
+//
+//  Created by 邢晓亮 on 2018/8/27.
+//  Copyright © 2018年 lanpin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPMainVC : UIViewController
+
+@end
