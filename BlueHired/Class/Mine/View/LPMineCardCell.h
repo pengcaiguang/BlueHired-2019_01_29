4e6c608e3d788a434ffd736a5e863a6c41a01bf8
xingxiaoliang <1018328249@qq.com>
2018-08-30 14:10:01 +0800

    我的界面

--- BlueHired/Class/Mine/View/LPMineCardCell.h
+++ BlueHired/Class/Mine/View/LPMineCardCell.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface LPMineCardCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIView *cellBgView;
 
 @end
--- BlueHired/Class/Mine/View/LPMineCardCell.m
+++ BlueHired/Class/Mine/View/LPMineCardCell.m
@@ -13,6 +13,40 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
+    
+    
+    NSArray *imgArray = @[@"workingRecord",@"salaryDetail",@"borrow",@"resignation",@"businessReview",@"interview",@"invitationReward",@"inviteFriends"];
+    NSArray *titleArray = @[@"工时记录",@"工资明细",@"蓝聘借支",@"离职通知",@"企业点评",@"面试预约",@"邀请奖励",@"邀请好友"];
+    
+    for (int i = 0; i < 8; i++) {
+        UIView *bgView = [[UIView alloc]init];
+        bgView.frame = CGRectMake(i%4 * SCREEN_WIDTH/4, floor(i/4)*86, SCREEN_WIDTH/4, 86);
+//        bgView.backgroundColor = randomColor;
+        bgView.userInteractionEnabled = YES;
+        bgView.tag = i;
+        UIImageView *imageView = [[UIImageView alloc]init];
+        imageView.frame = CGRectMake((SCREEN_WIDTH/4-37)/2, 15, 37, 37);
+//        imageView.backgroundColor = randomColor;
+        imageView.image = [UIImage imageNamed:imgArray[i]];
+        [bgView addSubview:imageView];
+        
+        UILabel *label = [[UILabel alloc]init];
+        label.frame = CGRectMake(0, 60, SCREEN_WIDTH/4, 20);
+//        label.backgroundColor = randomColor;
+        label.textColor = [UIColor colorWithHexString:@"#343434"];
+        label.font = [UIFont systemFontOfSize:12];
+        label.text = titleArray[i];
+        label.textAlignment = NSTextAlignmentCenter;
+        [bgView addSubview:label];
+        
+        [self.cellBgView addSubview:bgView];
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(touchBgView:)];
+        [bgView addGestureRecognizer:tap];
+    }
+}
+
+-(void)touchBgView:(UITapGestureRecognizer *)tap{
+    NSLog(@"%ld",[tap view].tag);
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
