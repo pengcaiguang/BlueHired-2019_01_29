78cf1cef3821085252fcbd0387c23047896a4a90
xingxiaoliang <1018328249@qq.com>
2018-09-25 19:43:16 +0800

    邀请奖励界面

---
+++ BlueHired/Class/Register/Controller/LPRegisterVC.h
@@ -0,0 +1,17 @@
+//
+//  LPRegisterVC.h
+//  BlueHired
+//
+//  Created by 邢晓亮 on 2018/9/25.
+//  Copyright © 2018 lanpin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LPRegisterVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BlueHired/Class/Register/Controller/LPRegisterVC.m
@@ -0,0 +1,33 @@
+//
+//  LPRegisterVC.m
+//  BlueHired
+//
+//  Created by 邢晓亮 on 2018/9/25.
+//  Copyright © 2018 lanpin. All rights reserved.
+//
+
+#import "LPRegisterVC.h"
+
+@interface LPRegisterVC ()
+
+@end
+
+@implementation LPRegisterVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.navigationItem.title = @"邀请奖励";
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
